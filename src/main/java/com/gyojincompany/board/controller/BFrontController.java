a7c4fcee7a945df77f58de6c69dc7d91e2909be9
JBEDU000 <JBEDU000@JBEDU00>
2022-04-26 12:10:28 +0900

    JSP MVC패턴 게시판 만들기

---
+++ src/main/java/com/gyojincompany/board/controller/BFrontController.java
@@ -0,0 +1,67 @@
+package com.gyojincompany.board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BFrontController
+ */
+@WebServlet("*.do")
+public class BFrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BFrontController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request, response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String viewPage = null;
+		
+		String uri = request.getRequestURI();//클라이언트가 요청한 URL 전부가져오기
+		String conPath = request.getContextPath();//context경로만 가져오기
+		String com = uri.substring(conPath.length());//전체 url에서 context 경로 길이만큼 빼기
+		
+		if(com.equals("/test.do")) {
+			request.setAttribute("id", "tiger");
+			
+			viewPage = "test.jsp";
+		} else if(com.equals("/delete.do")) {
+			
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+
+}
+
+
+
